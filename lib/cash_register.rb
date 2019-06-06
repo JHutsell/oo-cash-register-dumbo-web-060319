bc44c55aec76ec172859c63f3f6b881f41660aca
Justin Hutsell <hutselljustin@gmail.com>
2019-06-06 20:14:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
-
+class CashRegister 
+  attr_accessor :total
+  attr_reader :discount, :previous_total
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @previous_total = 0 
+  end
+  
+  def add_item(title, price, quantity = 1)
+    # @total += price
+    @previous_total = price * quantity 
+    quantity.times { @total += @previous_total } 
+  end
+  
+  def items 
+    @items
+  end
+  
+  
+end
